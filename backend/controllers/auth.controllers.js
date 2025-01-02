7c36ba201408bc53892ff9e764ae88840b8f799a
Mohd Aquib <akibkhan9984@gmail.com>
2025-01-02 18:44:22 +0530

    created a signup function in auth controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,9 +1,50 @@
-
+import UserModel from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
-    res.json({
-        message: 'User registered successfully'
-    })
+    try {
+        const { fullName, username, email, password } = req.body
+
+        const existingUser = await UserModel.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Username is already taken' });
+        }
+
+        const existingEmail = await UserModel.findOne({ email })
+        if (existingEmail) {
+            return res.status(400).json({ message: 'Email is already registered' });
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new UserModel({
+            fullName,
+            username,
+            email,
+            password: hashedPassword
+        })
+        if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res)
+            await newUser.save()
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg
+
+            })
+        } else {
+            res.status(400).json({ message: 'Failed to create user' })
+        }
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
 }
 
 export const login = async (req, res) => {
