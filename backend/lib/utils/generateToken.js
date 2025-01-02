c48125e8bda9c89629179cee684e784902de747a
Mohd Aquib <akibkhan9984@gmail.com>
2025-01-02 23:10:46 +0530

    fixed token options

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -4,8 +4,14 @@ export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
         expiresIn: "1d",
     })
-    console.log(token);
-    res.status(200)
+    console.log("Generated token", token);
+    const options = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: 86400000, // 1 day in milliseconds
+    };
+    res.status(201)
     .cookie("JWT", token, options)
     .json({ message: "Token generated successfully" });
 }
