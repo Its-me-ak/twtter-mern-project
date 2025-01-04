38294b632102c89108b4385d5b1a0f6281be6289
Mohd Aquib <akibkhan9984@gmail.com>
2025-01-04 19:11:04 +0530

    created notification model for send a notification msg to the user

---
+++ backend/models/notification.model.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose"
+
+const notificationSchema = new Schema({
+    from:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    to:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    type:{
+        type: String,
+        required: true,
+        enum: ['follow', 'like', 'comment']
+    },
+    read:{
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true,
+})
+
+const notificationModel = mongoose.model("Notification", notificationSchema)
+export default notificationModel
